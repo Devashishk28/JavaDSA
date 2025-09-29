efa9a9bced353ccf6725fbd4f2e359dc929202d9
Devashish Kubade <Devashishkubade2021@gmail.com>
2025-09-29 21:38:36 +0530

    Dsa day 4(q.2)

---
+++ Containsduplicate.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class Containsduplicate{
+    public static void containsduplicate(int nums[]){
+        HashMap<Integer,Integer> map=new HashMap<>();
+        int n=nums.length;
+
+        for(int i=0;i<n;i++){
+            if(map.containsKey(nums[i])){
+                map.put(nums[i],map.get(nums[i])+1);
+
+            }
+            else 
+                { 
+                    map.put(nums[i],1);
+                
+            }
+        }
+
+        for(Integer key:map.keySet()){
+            if(map.get(key)>n/n){
+                System.out.println(key);
+            }
+            else{
+                System.out.println("False");
+                break;
+            }
+        }
+    }
+
+    public static void main(String args[]){
+        int nums[]={1,2,3,4,5,6,8};
+        containsduplicate(nums);
+    }
+}
